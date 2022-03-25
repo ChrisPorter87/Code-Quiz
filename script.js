c3278996e776b9e64ebdd9901062bd81683cb532
Chris Porter <cap8095@gmail.com>
2022-03-25 10:38:30 -0400

    attempting a timer

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 var startQuiz = document.getElementById("start");
 
-startQuiz.addEventListener("click");
+startQuiz.addEventListener("click", setInterval);
+
+setInterval(myTimer, 60000);
+
+var counter = 60;
+function myTimer() {
+  console.log(
+    "You have been staring at your console for " + counter + "seconds"
+  );
+  counter++;
+}
