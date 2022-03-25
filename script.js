05d4284ec181ef44041f539ef8f73df615148a68
Chris Porter <cap8095@gmail.com>
2022-03-25 11:19:08 -0400

    timer is displayed and is triggered by click event

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 var startQuiz = document.getElementById("start");
 
-startQuiz.addEventListener("click", setInterval);
+startQuiz.addEventListener("click", myFunction);
 
-setInterval(myTimer, 60000);
-
-var counter = 60;
-function myTimer() {
-  console.log(
-    "You have been staring at your console for " + counter + "seconds"
-  );
-  counter++;
+var seconds = 60;
+var timer;
+function myFunction() {
+  if (seconds < 60) {
+    document.getElementById("timer").innerHTML = seconds;
+  }
+  if (seconds > 0) {
+    seconds--;
+  } else {
+    clearInterval(timer);
+    alert("The quiz is over!");
+  }
 }
+document.getElementById("start").onclick = function () {
+  if (!timer) {
+    timer = window.setInterval(function () {
+      myFunction();
+    }, 1000);
+  }
+};
+//When a key is pressed in the text area, update the timer using myFunction
+
+//If seconds are equal or greater than 0, countdown until 1 minute has passed
+//Else, clear the timer and alert user of how many words they type per minute
+
+document.getElementById("timer").innerHTML = "1:00";
