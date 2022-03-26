438aa3597899c327e3d2e5235e61b0c912dbf228
Chris Porter <cap8095@gmail.com>
2022-03-26 18:02:12 -0400

    making the question hide show

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ startQuiz.addEventListener("click", myFunction);
 var seconds = 60;
 var timer;
 function myFunction() {
+  document.getElementById("question1").style.display = "block";
+
   if (seconds < 60) {
     document.getElementById("timer").innerHTML = seconds;
   }
@@ -35,18 +37,75 @@ document.getElementById("start").onclick = function () {
   }
 };
 
+document.getElementById("question1").style.display = "none";
+document.getElementById("question2").style.display = "none";
+
+document.getElementById("question3").style.display = "none";
+
+document.getElementById("question4").style.display = "none";
+
 document.getElementById("timer").innerHTML = "1:00";
 
-document.getElementById("wrong").addEventListener("change", function (event) {
-  if (event.target && event.target.matches("input[type='radio']")) {
-    alert(
-      "That is incorrect. 5 seconds will be deducted from your remaining time!"
-    );
-  }
-});
+var list = document.getElementsByClassName("wrong");
+for (let item of list) {
+  //   item
+  item.addEventListener("change", function (event) {
+    if (event.target && event.target.matches("input[type='radio']")) {
+      alert(
+        "That is incorrect. 5 seconds will be deducted from your remaining time!"
+      );
+    }
+  });
+}
+var list = document.getElementsByClassName("correct");
+for (let item of list) {
+  //   item
+  item.addEventListener("change", function (event) {
+    if (event.target && event.target.matches("input[type='radio']")) {
+      alert("That is correct!");
+    }
+  });
+}
+// document.getElementsByClassName("wrong").forEach(function (elem) {
+//   elem.addEventListener("change", function (event) {
+//     if (event.target && event.target.matches("input[type='radio']")) {
+//       alert(
+//         "That is incorrect. 5 seconds will be deducted from your remaining time!"
+//       );
+//     }
+//   });
+// });
 
-document.getElementById("correct").addEventListener("change", function (event) {
-  if (event.target && event.target.matches("input[type='radio']")) {
-    alert("That is correct!");
-  }
-});
+var questions = ["question1", "question2", "question3", "question4"];
+var currentQuestion = 0;
+
+var list = document.getElementsByClassName("correct");
+for (let item of list) {
+  item.addEventListener("change", function (event) {
+    document.getElementById(questions[currentQuestion]).style.display = "none";
+    currentQuestion++;
+    document.getElementById(questions[currentQuestion]).style.display = "block";
+  });
+}
+// document
+//   .getElementsByClassName("correct")
+//   .addEventListener("change", function (event) {
+//     if (event.target && event.target.matches("input[type='radio']")) {
+//       alert("That is correct!");
+//       document.getElementById(questions[currentQuestion]).style.display =
+//         "none";
+//       currentQuestion++;
+//       document.getElementById(questions[currentQuestion]).style.display =
+//         "block";
+//     }
+//   });
+
+// .addEventListener("change", function (event) {
+//     if (event.target && event.target.matches("input[type='radio']")) {
+//       alert("That is correct!");
+//       document.getElementById(questions[currentQuestion]).style.display =
+//         "none";
+//       currentQuestion++;
+//       document.getElementById(questions[currentQuestion]).style.display =
+//         "block";
+//     }
